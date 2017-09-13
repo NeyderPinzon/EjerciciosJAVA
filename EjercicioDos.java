72518b47f9c24791da2b0947fd6ac77945e30362
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-09-13 16:03:42 -0300

    Segundo commit
    Ejercicio numero dos

---
+++ EjercicioDos.java
@@ -0,0 +1,19 @@
+package ejerciciodos;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioDos {
+
+    /**
+     * @param args the command line arguments
+     * Se debe pedir un nombre y se debe mostra el mismo nombre
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
