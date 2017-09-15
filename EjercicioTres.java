82e5231a537929f64d4e53a7605a2ecfe0873a49
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-09-15 01:42:06 -0300

    Primer commit
    Mostrando mensaje: ingrese su nombre y apellido

---
+++ EjercicioTres.java
@@ -0,0 +1,30 @@
+package ejerciciotres;
+
+import java.util.Scanner;
+
+public class EjercicioTres {
+
+    /**
+     * @param args the command line arguments
+     * se debe pedir el nombre y el apellido, luego mostrarlo en el siguiente mensaje: 
+     * "su nombre es xxxxx y xxxxx es su apellido
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre;
+        String apellido;
+        Scanner entradaPorTeclado = new Scanner(System.in);
+        System.out.println("Por favor ingrese su Nombre: ");
+        nombre = entradaPorTeclado.nextLine();
+        System.out.println("Por favor ingrese su apellido: ");
+        apellido = entradaPorTeclado.nextLine();
+        
+        
+        
+        
+        
+        
+    }
+    
+}
