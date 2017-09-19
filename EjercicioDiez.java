1d91a988177a738d20ab58bb45477415240216a4
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-09-19 15:34:38 -0300

    Segundo Commit

--- EjercicioDiez.java
+++ EjercicioDiez.java
@@ -3,28 +3,33 @@ package ejerciciodiez;
 import java.util.Scanner;
 
 public class EjercicioDiez {
-
     /**
-     * @param args the command line arguments
-     * se ingresa la cantidad de ganadores de la loteria nacional y el monto 
+     * 
+     * @param args
+     *  se ingresa la cantidad de ganadores de la loteria nacional y el monto 
      * total del premio, informar cuanto dnero le corresponde a cada uno
      */
+
     public static void main(String[] args) {
         // TODO code application logic here
-        int ganadores;
-        int montoTotal;
-        int porUno;
+        String DatoAux;
+        Integer montoTotal;
+        Double porUno;
+        Double total;
         Scanner entradaPorTeclado = new Scanner(System.in);
-        System.out.println("Total de ganadores: ");
-        ganadores = entradaPorTeclado.nextInt();
-        System.out.println("Cantidad total de la loteria: ");
-        montoTotal = entradaPorTeclado.nextInt();
-        porUno = montoTotal/ganadores;//tener cuidado de escribir las expreciones bien, como es y no alreves
-        System.out.println("A cada ganador le corresponde un monto de: "+porUno);
         
+        System.out.println("Total de Ganadores: ");
+        DatoAux = entradaPorTeclado.next();
+        montoTotal = Integer.parseInt(DatoAux);
         
-        //System.out.println("La división es " + numero1 + " / " + numero2 + " = " + resultado);
+        System.out.println("Dinero total Ganado: ");
+        DatoAux = entradaPorTeclado.next();
+        porUno = Double.parseDouble(DatoAux);
         
-    }
+        total= porUno/montoTotal;
+        System.out.println("A cada Ganador le corresponde: " + total);
+        // aprendiendo a parsear, debo estudiar bastante esto ya que se me dificulta bastante
+        }
+    
     
 }
