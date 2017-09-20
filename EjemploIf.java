ed87e140ac673a41e2c9e88f6ef2a1a27f5ade47
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-09-20 15:15:53 -0300

    Segundo Commit
    ejemplos de If- Else

--- EjemploIf.java
+++ EjemploIf.java
@@ -14,19 +14,32 @@ public class EjemploIf {
         //Defino las variables        
         String nombre;
         String clave;
+        String sexo;
         
         //tomo datos
         System.out.println("Ingresse nombre: ");
         nombre = entrada.next();
+        
         System.out.println("Ingresse clave: ");
         clave = entrada.next();
         
+        System.out.println("Ingrese sexo: ");
+        sexo = entrada.next();
+        
+        //Instruccion if-else
         if(nombre.equals("Admin")){
             System.out.println("Bienvenido Admin");
         }
         else{
-            System.out.println("Bienvenido   "+ nombre); 
+            System.out.println("Bienvenido   "+ nombre);
+            
+        }        
+        //Ingresa M para masculino, F para femenino
+        if (sexo.equals("f")){
+            System.out.println("Feliz dia de la mujer");
         }
+        
+        
         
     }
     
