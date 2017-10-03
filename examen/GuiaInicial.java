773582d00ff8a6649cac9ed8737a30e0ac6c8fb9
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-10-03 15:11:37 -0300

    Funciones

---
+++ examen/GuiaInicial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen;
+
+/**
+ *
+ * @author alumno
+ */
+public class GuiaInicial {
+    
+    public static void saludar() {
+        // TODO code application logic here
+        //public: cualquiera la puede ver
+        //static: estaticco
+        //void: no retorna ningun valor
+        
+        System.out.println("Buenas tardes"); 
+        
+    }
+    
+}
